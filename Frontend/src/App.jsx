4efebeba1be824a2705be8c7aac5b4fa66a221ad
navyumm <navyum12@gmail.com>
2026-08-18 12:59:02 +0530

    setting up frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/random-user')
+      .then((response) => {
+        setUsers(response.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error)
+      })
+  }, [])
+
+
+  // useEffect(()=>{},[])
+  
+
+  return (
+    <>
+    <h1>frontend and backend connection</h1>
+    <h2>Number of users: {users.length}</h2>
+    {
+      users.map((user, index) => (
+        <div key={index}>
+          <h3>{user.name}</h3>
+          <p>{user.age}</p>
+        </div>
+      ))
+    }
+    </>
+  )
+}
+
+export default App
